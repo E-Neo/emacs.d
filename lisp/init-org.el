53e0d205d068b6d5f08c341227a6ace0a3e96edc
E-Neo <e-neo@qq.com>
2018-10-31 14:53:49 +0800

    fix minted ^^I error

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -9,7 +9,7 @@
 (add-to-list 'org-latex-packages-alist '("" "minted"))
 (setq org-latex-listings 'minted)
 (setq org-latex-pdf-process
-      '("latexmk -pdflatex='xelatex -shell-escape -interaction nonstopmode' -pdf -f %f && latexmk -bibtex -c"))
+      '("latexmk -pdflatex='xelatex -8bit -shell-escape -interaction nonstopmode' -pdf -f %f && latexmk -bibtex -c"))
 (setq-default org-format-latex-options
 	      (plist-put org-format-latex-options :scale 2.0))
 
