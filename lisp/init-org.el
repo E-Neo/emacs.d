6307afcf5ef2cf9fb5da2419e8433684d7aa9b49
E-Neo <e-neo@qq.com>
2018-11-28 15:00:05 +0800

    do not use minted globally

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,8 +6,8 @@
 (require 'ox-latex)
 
 
-(add-to-list 'org-latex-packages-alist '("" "minted"))
-(setq org-latex-listings 'minted)
+;; If you need minted please use file variables:
+;; org-latex-listings: 'minted
 (setq org-latex-pdf-process
       '("latexmk -pdflatex='xelatex -8bit -shell-escape -interaction nonstopmode' -pdf -f %f && latexmk -bibtex -c"))
 (setq-default org-format-latex-options
