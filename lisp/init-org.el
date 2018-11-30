af961e89c8d735ff4555c168563a50900fd56c7c
E-Neo <e-neo@qq.com>
2018-11-30 11:20:22 +0800

    minted

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,10 +6,12 @@
 (require 'ox-latex)
 
 
-;; If you need minted please use file variables:
-;; org-latex-listings: 'minted
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+(setq org-latex-listings 'minted)
 (setq org-latex-pdf-process
-      '("latexmk -pdflatex='xelatex -8bit -shell-escape -interaction nonstopmode' -pdf -f %f && latexmk -bibtex -c"))
+      '("latexmk \
+-pdflatex='xelatex -8bit -shell-escape -interaction nonstopmode' \
+-pdf -f %f && latexmk -bibtex -c && rm -rf _minted-%b"))
 (setq-default org-format-latex-options
 	      (plist-put org-format-latex-options :scale 2.0))
 
