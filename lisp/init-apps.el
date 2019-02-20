9513fce7661a7332d88e45c7b3bbe85fa4044da9
E-Neo <e-neo@qq.com>
2019-02-20 22:00:27 +0800

    remove pyim, use fcitx instead

--- lisp/init-apps.el
+++ lisp/init-apps.el
@@ -8,14 +8,6 @@
   (emms-default-players))
 
 
-(use-package pyim
-  :config
-  (use-package pyim-basedict
-    :ensure nil
-    :config (pyim-basedict-enable))
-  (setq default-input-method "pyim"))
-
-
 (provide 'init-apps)
 
 ;;; init-apps.el ends here
