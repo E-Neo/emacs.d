7233e98480008c13df58a753096eaeb0b54c2e32
E-Neo <e-neo@qq.com>
2026-01-22 23:06:09 +0800

    AI: Add gptel

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -8,6 +8,35 @@
   :custom
   (aidermacs-default-chat-mode 'architect))
 
+(use-package gptel
+  :hook ((gptel-mode . gptel-highlight-mode))
+  :bind (("C-c <return>" . gptel-send)
+         ("C-c C-<return>" . gptel-menu)
+         ("C-c C-g" . gptel-abort))
+  :config
+  (defun ai--load-skill (name)
+    (let ((file-path (file-name-concat user-emacs-directory
+                                       "skills/"
+                                       (symbol-name name)
+                                       "SKILL.md")))
+      (with-temp-buffer
+        (insert-file-contents file-path)
+        (buffer-string))))
+
+  (setq-default gptel-model 'deepseek-ai/deepseek-v3.2
+                gptel-backend
+                (gptel-make-openai "Open Source Models"
+                  :host "integrate.api.nvidia.com"
+                  :endpoint "/v1/chat/completions"
+                  :key #'gptel-api-key-from-auth-source
+                  :stream t
+                  :models '(deepseek-ai/deepseek-v3.2
+                            qwen/qwen3-coder-480b-a35b-instruct)))
+
+  (gptel-make-preset 'shell
+    :backend "Open Source Models"
+    :model 'qwen/qwen3-coder-480b-a35b-instruct
+    :system (lambda () (ai--load-skill 'shell))))
 
 (provide 'init-ai)
 
---
+++ skills/shell/SKILL.md
@@ -0,0 +1,16 @@
+---
+name: shell
+description: Generate shell command lines as requested.
+---
+
+# Shell command lines generator
+
+## Overview
+
+You are a command line helper. Generate command lines that do what is requested, without any additional description or explanation. Generate ONLY the command, so the user can run it directly.
+
+## Examples
+
+User request: "What time is it?".
+Your reply: "date".
+Note that you must only reply the four letter word "date", without any other symbols.
